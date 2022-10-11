6d282830510985efe7b6a3729f104a617b1b0aab
Eric Cleptonsilva <ericcleptonsilva@gmail.com>
2022-10-11 19:31:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./styles/global.css";
+export function App() {
+  return (
+    <div className="bg-zinc-800 w-full h-auto">
+      <h1 className="text-lg text-yellow-900">oi</h1>
+    </div>
+  );
+}
