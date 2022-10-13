88ca6aebcbef4b45a2d4dc997f2d222546139331
Eric Cleptonsilva <ericcleptonsilva@gmail.com>
2022-10-13 04:02:00 -0300

    Todos os componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./styles/global.css";
 export function App() {
   return (
-    <div className="bg-zinc-800 w-full h-auto">
-      <h1 className="text-lg text-yellow-900">oi</h1>
+    <div className="bg-zinc-800 w-full h-auto flex items-center">
+      <h1 className="text-2xl  font-bold  text-yellow-900">ola</h1>
     </div>
   );
 }
